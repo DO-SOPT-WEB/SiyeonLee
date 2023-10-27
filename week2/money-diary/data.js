0f218ec142729da4e765a117a259b6ab8a356942
SynthiaLee <seanne0801@gmail.com>
2023-10-27 19:53:41 +0900

    feat: 가계부 기본과제 완성

---
+++ week2/money-diary/data.js
@@ -0,0 +1,27 @@
+export const INIT_BALANCE = 0;
+export const HISTORY_LIST = [
+  {
+    category: "Shopping",
+    title: "ZARA",
+    price: 49000,
+    type: "price_withdraw",
+  },
+  {
+    category: "Paycheck",
+    title: "EduPlex",
+    price: 300000,
+    type: "price_deposit",
+  },
+  {
+    category: "Food",
+    title: "Subway",
+    price: 11000,
+    type: "price_withdraw",
+  },
+  {
+    category: "Drink",
+    title: "Starbucks",
+    price: 7000,
+    type: "price_withdraw",
+  },
+];
