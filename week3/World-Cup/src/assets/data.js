2edd1fe9a0a89da3418faa6a6165660acaf3e00f
SynthiaLee <seanne0801@gmail.com>
2023-11-09 14:15:59 +0900

    feat: 데이터 상수파일 저장

---
+++ week3/World-Cup/src/assets/data.js
@@ -0,0 +1,94 @@
+const DRINK_LIST = [
+  {
+    name: "바닐라 라떼",
+    category: ["coffee", "sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/vanillalatte.jpg",
+  },
+  {
+    name: "아이스 바닐라 라떼",
+    category: ["coffee", "sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_vanillalatte.jpeg",
+  },
+  {
+    name: "아메리카노",
+    category: ["coffee", "non-sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/americano.jpg",
+  },
+  {
+    name: "아이스 아메리카노",
+    category: ["coffee", "non-sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_americano.jpg",
+  },
+  {
+    name: "카페라떼",
+    category: ["coffee", "dairy", "hot"],
+    img: "week3/World-Cup/src/assets/images/caffelatte.jpg",
+  },
+  {
+    name: "아이스 카페라떼",
+    category: ["coffee", "dairy", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_caffelatte.jpg",
+  },
+  {
+    name: "자몽 허니 블랙티",
+    category: ["tea", "sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/jaheoble.jpg",
+  },
+  {
+    name: "아이스 자몽 허니 블랙티",
+    category: ["tea", "sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_jaheoble.jpg",
+  },
+  {
+    name: "우롱차",
+    category: ["tea", "non-sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/oolongtea.jpg",
+  },
+  {
+    name: "아이스 우롱차",
+    category: ["tea", "non-sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_oolongtea.jpg",
+  },
+  {
+    name: "밀크티",
+    category: ["tea", "dairy", "hot"],
+    img: "week3/World-Cup/src/assets/images/milktea.jpg",
+  },
+  {
+    name: "아이스 밀크티",
+    category: ["tea", "dairy", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_milktea.jpg",
+  },
+  {
+    name: "흑임자 라떼",
+    category: ["halmi", "sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/blacksesame.avif",
+  },
+  {
+    name: "아이스 흑임자 라떼",
+    category: ["halmi", "sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_balcksesame.png",
+  },
+  {
+    name: "쑥 라떼",
+    category: ["halmi", "non-sweet", "hot"],
+    img: "week3/World-Cup/src/assets/images/ssooklatte.jpg",
+  },
+  {
+    name: "아이스 쑥 라떼",
+    category: ["halmi", "non-sweet", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_ssooklatte.jpg",
+  },
+  {
+    name: "고구마 라떼",
+    category: ["halmi", "dairy", "hot"],
+    img: "week3/World-Cup/src/assets/images/sweetpotato.jpg",
+  },
+  {
+    name: "아이스 고구마 라떼",
+    category: ["halmi", "dairy", "cold"],
+    img: "week3/World-Cup/src/assets/images/iced_sweetpotato.jpg",
+  },
+];
+
+export default DRINK_LIST;
