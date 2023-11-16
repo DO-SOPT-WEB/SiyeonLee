45efa6cbf9fff59811891a4b439c927c06fb3968
SynthiaLee <seanne0801@gmail.com>
2023-11-17 05:14:55 +0900

    feat: 시작 화면시 로그인페이지로 자동 리다이렉트

--- week4/Login/src/components/Router.jsx
+++ week4/Login/src/components/Router.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import MyPage from "./MyPage";
+import { Navigate } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/mypage/:userId" element={<MyPage />} />
